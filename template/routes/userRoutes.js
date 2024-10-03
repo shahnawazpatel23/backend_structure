5f0d055626cc39678a1293b4a0cf1b4577b85db9
shahnawaz patel <shahnawazpatel23@gmail.com>
2024-10-03 06:16:47 +0530

    gitignore add

--- template/routes/userRoutes.js
+++ template/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { } = require("../controllers/user.controller")
+const {getUser,getAdmin } = require("../controllers/user.controller")
 const verifyToken = require("../middlewares/auth.middleware")
 const roleMiddleware = require("../middlewares/role.middleware")
 
@@ -7,8 +7,8 @@ const router = express.Router();
 
 // here we'll use middleware for controlled access to routes
 // using middleware , we would allow role based access control
-router.get('/user',verifyToken,roleMiddleware("role1","role2"),)
-router.get('/admin',verifyToken,roleMiddleware("role1"),)
+router.get('/user',verifyToken,roleMiddleware("role1","role2"),getUser)
+router.get('/admin',verifyToken,roleMiddleware("role1"),getAdmin)
 
 
 module.exports = router
